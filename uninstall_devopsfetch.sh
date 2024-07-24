9f5c0e05430683821e362d20a3ad5d3ebe111ab1
dominic-source <chinonsodomnic@gmail.com>
2024-07-24 22:55:47 +0100

    update uninstallation

--- uninstall_devopsfetch.sh
+++ uninstall_devopsfetch.sh
@@ -2,26 +2,43 @@
 sudo echo "" 
 echo "Uninstalling DevOpsFetch..."
 
+# Function to check if systemctl exists
+function use_systemctl {
+    command -v systemctl &> /dev/null
+}
 
-# Stop the service timer
-sudo systemctl stop devopsfetch.timer &> /dev/null
+# Stop and disable the service/timer
+if use_systemctl; then
+    # Stop the service timer
+    sudo systemctl stop devopsfetch.timer &> /dev/null
 
-# Disable the service
-sudo systemctl disable devopsfetch.timer &> /dev/null
+    # Disable the service timer
+    sudo systemctl disable devopsfetch.timer &> /dev/null
 
-# Remove the systemd service file
+    # Stop the service
+    sudo systemctl stop devopsfetch.service &> /dev/null
+
+    # Disable the service
+    sudo systemctl disable devopsfetch.service &> /dev/null
+else
+    # Stop the service timer
+    sudo service devopsfetch.timer stop &> /dev/null
+
+    # Disable the service timer
+    sudo service devopsfetch.timer disable &> /dev/null
+
+    # Stop the service
+    sudo service devopsfetch stop &> /dev/null
+
+    # Disable the service
+    sudo service devopsfetch disable &> /dev/null
+fi
+
+# Remove the systemd timer file
 sudo rm /etc/systemd/system/devopsfetch.timer &> /dev/null
-
-
-# Stop the service
-sudo systemctl stop devopsfetch.service &> /dev/null
-
-# Disable the service
-sudo systemctl disable devopsfetch.service &> /dev/null
 
 # Remove the systemd service file
 sudo rm /etc/systemd/system/devopsfetch.service &> /dev/null
-
 
 # Reload systemd to apply changes
 sudo systemctl daemon-reload
